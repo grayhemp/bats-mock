1cca0f0dd2db00c7373a60f86f304532b8e531d1
Sergey Konoplev <gray.ru@gmail.com>
2018-04-12 23:05:55 -0700

    Added all the planned functionality, docs and tests, more refactoring

--- README.md
+++ README.md
@@ -2,23 +2,145 @@
 
 [![Build Status](https://travis-ci.org/grayhemp/bats-mock.svg?branch=master)](https://travis-ci.org/grayhemp/bats-mock)
 
-`bats-mock` is a helper library providing mocking functionality for
-[Bats][bats].
+A [Bats][bats-core] helper library providing mocking functionality.
 
-<!-- TODO(grayhemp): Add description -->
+```bash
+load bats-mock
 
-<!-- TODO(grayhemp): Add dependencies -->
+@test "postgres.sh starts Postgres" {
+  mock="$(mock_create)"
+  mock_set_side_effect "${mock}" 'touch /tmp/postgres_started'
 
+  _POSTGRES="${mock}" run postgres.sh
+
+  [[ "${status}" -eq 0 ]]
+  [[ "$(mock_get_call_num ${mock})" -eq 1 ]]
+  [[ "$(mock_get_call_user ${mock})" = 'postgres' ]]
+  [[ "$(mock_get_call_args ${mock})" =~ -D\ /var/lib/postgresql ]]
+  [[ "$(mock_get_call_env ${mock} PGPORT)" -eq 5432 ]]
+}
+```
+
+## Testing
+
+```bash
+./build test
+```
+
+Requires [Bats][bats-core] to be installed and in `PATH`.
+
+Optionally accepts the `BINDIR` env.
 
 ## Installation
 
+```bash
+./build install
+```
 
-## Loading
-
+Optionally accepts `PREFIX` and `LIBDIR` envs.
 
 ## Usage
 
+### `mock_create`
 
-<!-- REFERENCES -->
+```bash
+mock_create
+```
 
-[bats]: https://github.com/sstephenson/bats
+Creates a mock program with a unique name in `BATS_TMPDIR` and outputs
+its path.
+
+The mock tracks calls and collects their properties. The collected
+data is accessible using methods described below.
+
+### `mock_set_status`
+
+```bash
+mock_set_status <mock> <status> [<n>]
+```
+
+Sets the exit status of the mock.
+
+`0` status is set by default when mock is created.
+
+If `n` is specified the status will be returned on the `n`-th
+call. The call indexing starts with `1`. Multiple invocations can be
+used to mimic complex status sequences.
+
+### `mock_set_output`
+
+```bash
+mock_set_return_value <mock> (<output>|-) [<n>]
+```
+
+Sets the output of the mock.
+
+The mock outputs nothing by default.
+
+If the output is specified as `-` then it is going to be read from
+`STDIN`.
+
+The optional `n` argument behaves similarly to the one of
+`mock_set_exit_code`.
+
+### `mock_set_side_effect`
+
+```bash
+mock_set_side_effect <mock> (<side_effect>|-) [<n>]
+```
+
+Sets the side effect of the mock. The side effect is a bash code to be
+sourced by the mock when it is called.
+
+No side effect is set by default.
+
+If the side effect is specified as `-` then it is going to be read
+from `STDIN`.
+
+The optional `n` argument behaves similarly to the one of
+`mock_set_exit_code`.
+
+### `mock_get_call_num`
+
+```bash
+mock_get_call_num <mock>
+```
+
+Returns the number of times the mock was called.
+
+### `mock_get_call_user`
+
+```bash
+mock_get_call_user <mock> [<n>]
+```
+
+Returns the user the mock was called with the `n`-th time. If no `n`
+is specified then assuming the last call.
+
+It requires the mock to be called at least once.
+
+### `mock_get_call_args`
+
+```bash
+mock_get_call_args <mock> [<n>]
+```
+
+Returns the arguments line the mock was called with the `n`-th
+time. If no `n` is specified then assuming the last call.
+
+It requires the mock to be called at least once.
+
+### `mock_get_call_env`
+
+```bash
+mock_get_call_env <mock> <variable> [<n>]
+```
+
+Returns the value of the environment variable the mock was called with
+the `n`-th time. If no `n` is specified then assuming the last call.
+
+It requires the mock to be called at least once.
+
+<!-- Links -->
+
+[bats-core]: https://github.com/bats-core/bats-core
--- src/bats-mock.bash
+++ src/bats-mock.bash
@@ -1,9 +1,223 @@
 #!/usr/bin/env bash
 #
-#
+# A Bats helper library providing mocking functionality
+
+# Creates a mock program
+# Globals:
+#   BATS_TMPDIR
+# Outputs:
+#   STDOUT: Path to the mock
+mock_create() {
+  local index
+  index="$(find ${BATS_TMPDIR} -name bats-mock.$$.* | wc -l | tr -d ' ')"
+  local mock
+  mock="${BATS_TMPDIR}/bats-mock.$$.${index}"
+
+  echo -n 0 > "${mock}.call_num"
+  echo -n 0 > "${mock}.status"
+  echo -n '' > "${mock}.output"
+  echo -n '' > "${mock}.side_effect"
+
+  cat <<EOF > "${mock}"
+#!/usr/bin/env bash
 
 set -e
 
-mock_program() {
-  exit 0
+mock="${mock}"
+
+call_num="\$(( \$(cat \${mock}.call_num) + 1 ))"
+echo "\${call_num}" > "\${mock}.call_num"
+
+echo "\${_USER:-\$(id -un)}" > "\${mock}.user.\${call_num}"
+
+echo "\$@" > "\${mock}.args.\${call_num}"
+
+for var in \$(compgen -e); do
+  declare -p "\${var}"
+done > "\${mock}.env.\${call_num}"
+
+if [[ -e "\${mock}.output.\${call_num}" ]]; then
+  cat "\${mock}.output.\${call_num}"
+else
+  cat "\${mock}.output"
+fi
+
+if [[ -e "\${mock}.side_effect.\${call_num}" ]]; then
+  source "\${mock}.side_effect.\${call_num}"
+else
+  source "\${mock}.side_effect"
+fi
+
+if [[ -e "\${mock}.status.\${call_num}" ]]; then
+  exit "\$(cat \${mock}.status.\${call_num})"
+else
+  exit "\$(cat \${mock}.status)"
+fi
+EOF
+  chmod +x "${mock}"
+
+  echo "${mock}"
+}
+
+# Sets the exit status of the mock
+# Arguments:
+#   1: Path to the mock
+#   2: Status
+#   3: Index of the call, optional
+mock_set_status() {
+  local mock="${1?'Mock must be specified'}"
+  local status="${2?'Status must be specified'}"
+  local n="$3"
+
+  if [[ -n "${n}" ]]; then
+    echo "${status}" > "${mock}.status.${n}"
+  else
+    echo "${status}" > "${mock}.status"
+  fi
+}
+
+# Sets the output of the mock
+# Arguments:
+#   1: Path to the mock
+#   2: Output or - for STDIN
+#   3: Index of the call, optional
+# Inputs:
+#   STDIN: Output if 2 is -
+mock_set_output() {
+  local mock="${1?'Mock must be specified'}"
+  local output="${2?'Output must be specified'}"
+  local n="$3"
+
+  if [[ "${output}" = '-' ]]; then
+    output="$(cat -)"
+  fi
+
+  if [[ -n "${n}" ]]; then
+    echo -e "${output}" > "${mock}.output.${n}"
+  else
+    echo -e "${output}" > "${mock}.output"
+  fi
+}
+
+# Sets the side effect of the mock
+# Arguments:
+#   1: Path to the mock
+#   2: Side effect or - for STDIN
+#   3: Index of the call, optional
+# Inputs:
+#   STDIN: Side effect if 2 is -
+mock_set_side_effect() {
+  local mock="${1?'Mock must be specified'}"
+  local side_effect="${2?'Side effect must be specified'}"
+  local n="$3"
+
+  if [[ "${side_effect}" = '-' ]]; then
+    side_effect="$(cat -)"
+  fi
+
+  if [[ -n "${n}" ]]; then
+    echo -e "${side_effect}" > "${mock}.side_effect.${n}"
+  else
+    echo -e "${side_effect}" > "${mock}.side_effect"
+  fi
+}
+
+# Returns the number of times the mock was called
+# Arguments:
+#   1: Path to the mock
+# Outputs:
+#   STDOUT: Number of calls
+mock_get_call_num() {
+  local mock="${1?'Mock must be specified'}"
+
+  echo "$(cat ${mock}.call_num)"
+}
+
+# Returns the user the mock was called with
+# Arguments:
+#   1: Path to the mock
+#   2: Index of the call, optional
+# Returns:
+#   1: If mock is not called enough times
+# Outputs:
+#   STDOUT: User name
+#   STDERR: Corresponding error message
+mock_get_call_user() {
+  local mock="${1?'Mock must be specified'}"
+  local n="$2"
+
+  local call_num
+  call_num="$(cat ${mock}.call_num)"
+
+  local n="${2:-${call_num}}"
+  if [[ "${n}" -eq 0 ]]; then
+    n=1
+  fi
+
+  if [[ "${n}" -gt "${call_num}" ]]; then
+    echo "Mock must be called at least ${n} time(s)" >&2
+    exit 1
+  fi
+
+  echo "$(cat ${mock}.user.${n})"
+}
+
+# Returns the arguments line the mock was called with
+# Arguments:
+#   1: Path to the mock
+#   2: Index of the call, optional
+# Returns:
+#   1: If mock is not called enough times
+# Outputs:
+#   STDOUT: Arguments line
+#   STDERR: Corresponding error message
+mock_get_call_args() {
+  local mock="${1?'Mock must be specified'}"
+
+  local call_num
+  call_num="$(cat ${mock}.call_num)"
+
+  local n="${2:-${call_num}}"
+  if [[ "${n}" -eq 0 ]]; then
+    n=1
+  fi
+
+  if [[ "${n}" -gt "${call_num}" ]]; then
+    echo "Mock must be called at least ${n} time(s)" >&2
+    exit 1
+  fi
+
+  echo "$(cat ${mock}.args.${n})"
+}
+
+# Returns the value of the environment variable the mock was called with
+# Arguments:
+#   1: Path to the mock
+#   2: Variable name
+#   3: Index of the call, optional
+# Returns:
+#   1: If mock is not called enough times
+# Outputs:
+#   STDOUT: Variable value
+#   STDERR: Corresponding error message
+mock_get_call_env() {
+  local mock="${1?'Mock must be specified'}"
+  local var="${2?'Variable name must be specified'}"
+
+  local call_num
+  call_num="$(cat ${mock}.call_num)"
+
+  local n="${3:-${call_num}}"
+  if [[ "${n}" -eq 0 ]]; then
+    n=1
+  fi
+
+  if [[ "${n}" -gt "${call_num}" ]]; then
+    echo "Mock must be called at least ${n} time(s)" >&2
+    exit 1
+  fi
+
+  source "${mock}.env.${n}"
+
+  echo "${!var}"
 }
